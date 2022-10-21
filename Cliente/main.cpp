0e1f8e5d8db2091f6a31502348d8be456ac0cdcc
Link0215 <74025450+Link0215@users.noreply.github.com>
2022-10-20 21:15:40 -0500

    Cliente puede mandar mensajes
    Cliente puede mandar mensajes

---
+++ Cliente/main.cpp
@@ -0,0 +1,100 @@
+#include <SFML/Network.hpp>
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <winsock.h>
+
+using uint16 = unsigned short;
+using uint32 = unsigned int;
+
+using int16 = short;
+using int32 = int;
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::list;
+using std::size_t;
+using std::optional;
+using std::quoted;
+using std::string;
+using std::stringstream;
+
+using sf::UdpSocket;
+using sf::Socket;
+using sf::IpAddress;
+
+const uint16 APP_PORT = 35001;
+const uint32 MESSAGE_BUFFER_SIZE = 128;
+
+UdpSocket g_clientSocket;
+std::optional<sf::IpAddress> g_serverIP;
+std::optional<sf::IpAddress> g_clientIP;
+
+void runUdpClient(uint16 port)
+{
+	do
+	{
+		cout << "Escribe el nombre del servidor o IP: ";
+		cin >> g_serverIP;
+	} while (!g_serverIP.has_value());
+	do
+	{
+		cout << "Escribe tu IP: ";
+		cin >> g_clientIP;
+	} while (!g_clientIP.has_value());
+
+}
+
+void waitForMessage()
+{
+	vector<char> inBufferData;
+	inBufferData.resize(MESSAGE_BUFFER_SIZE);
+	rsize_t receiveMessageSize;
+	std::optional<sf::IpAddress> sender;
+	uint16 port;
+
+	Socket::Status retValue =
+		g_clientSocket.receive(&inBufferData[0],
+								inBufferData.size(),
+								receiveMessageSize,
+								sender,
+								port);
+	if (retValue != Socket::Done)
+	{//Error
+		return;
+	}
+	cout << "Mensaje recibido del: "
+		<< g_serverIP.value() << ": "
+		<< quoted(inBufferData.data()) << endl;
+}
+
+void sendMessage(const string& message)
+{
+	int lenght = message.length();
+	int size =
+
+		g_clientSocket.send(message.data(),
+							message.length() + 1,
+							g_clientIP.value(),
+							APP_PORT);
+}
+
+int main()
+{
+	uint32 numMessagesReceived = 0;
+	std::string messageToUsers;
+	runUdpClient(APP_PORT);
+
+	while (true)
+	{
+		++numMessagesReceived;
+		std::getline(cin, messageToUsers);
+		sendMessage(messageToUsers);
+		waitForMessage();
+	}
+	return 0;
+}
--- CmakeAndSFML/CmakeAndSFML.cpp
+++ CmakeAndSFML/CmakeAndSFML.cpp
@@ -1,31 +1,140 @@
-#include <cstdlib>
+#include <SFML/Network.hpp>
+
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <Windows.h>
+#include <list>
+
+
+using uint16 = unsigned short;
+using uint32 = unsigned int;
+
+using int16 = short;
+using int32 = int;
 
 using std::cout;
-using std::endl;
 using std::cin;
+using std::endl;
+using std::vector;
+using std::list;
+using std::size_t;
+using std::optional;
+using std::quoted;
+using std::string;
+using std::stringstream;
 
-void runTcpServer(unsigned short port);
-void runTcpClient(unsigned short port);
-void runUdpServer(unsigned short port);
-void runUdpClient(unsigned short port);
+using sf::UdpSocket;
+using sf::Socket;
+using sf::IpAddress;
+
+const uint16 APP_PORT = 35001;
+const uint32 MESSAGE_BUFFER_SIZE = 128;
+static string host;
+
+UdpSocket g_serverSocket;
+
+//void ping();
+
+class NertworkClient
+{
+public:
+	NertworkClient() = default;
+	~NertworkClient() = default;
+
+	bool operator==(const NertworkClient& other)
+	{
+		return (m_ip.value().toInteger() == other.m_ip.value().toInteger()
+			&& m_port == other.m_port);
+	}
+	bool operator!=(const NertworkClient& other)
+	{
+		return !this->operator==(other);
+	}
+
+public:
+	optional<IpAddress> m_ip;
+	uint16 m_port;
+};
+
+vector <NertworkClient> g_clientlist;
+
+void runUdpServer(uint16 port)
+{
+	if (g_serverSocket.bind(port) != Socket::Done)
+	{
+		return;
+	}
+
+	cout << "Servidor esta escuchando en el puerto: " << port << endl;
+}
+
+void waitForMessage()
+{
+	NertworkClient client;
+	vector<char> inBufferData;
+	std::optional<sf::IpAddress> sender;
+	uint16 senderPort;
+	inBufferData.resize(MESSAGE_BUFFER_SIZE);
+	rsize_t receiveMessageSize;
+
+	Socket::Status retValue =
+	g_serverSocket.receive(&inBufferData[0],
+							inBufferData.size(),
+							receiveMessageSize,
+							sender,
+							senderPort);
+	client.m_ip = sender;
+	client.m_port = senderPort;
+	g_clientlist.push_back(client);
+	if (retValue != Socket::Done)
+	{//Error
+		return;
+	}
+	cout << "Mensaje recibido del cliente: "
+		<< client.m_ip.value() << ": "
+		<< quoted(inBufferData.data()) << endl;
+	/*ping();*/
+	cout << endl;
+}
+
+void sendMessage(const string &message)
+{
+	NertworkClient client;
+	int lenght = message.length();
+	int size =
+
+	g_serverSocket.send(message.data(),
+						message.length() + 1,
+						g_clientlist[0].m_ip.value(),
+						g_clientlist[0].m_port);
+}
+
+//void ping()
+//{
+//	system("cls");
+//	cout << "Host: ";
+//	host = "192.168.1.71";
+//	system(("ping " + host).c_str());
+//	cout << endl;
+//	return;
+//}
 
 int main()
 {
-    // Elija un puerto arbitrario para abrir sockets
-    const unsigned short port = 50001;
+	uint32 numMessagesReceived = 0;
+	std::stringstream messageToUsers;
+	runUdpServer(APP_PORT);
 
-    //Cliente o servidor
-    char who;
-    cout << "Quieres ser un servidor (s) o un cliente (c)? " << endl;
-    cin >> who;
-    if (who == 's')
-    {
-        runUdpServer(port);
-    }
-    else
-    {
-        runUdpClient(port);
-    }
-    return EXIT_SUCCESS;
+	while (true)
+	{
+		waitForMessage();
+		++numMessagesReceived;
+		messageToUsers.str("");
+		messageToUsers.clear();
+		messageToUsers << "Mensaje al cliente num: " << numMessagesReceived;
+		sendMessage(messageToUsers.str());
+	}
+	return 0;
 }
